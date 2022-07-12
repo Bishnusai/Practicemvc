efa79c67e912286d47e672c3cb4c5c721153fb5a
Bishnusai <20mca078.bishnubehera@giet.edu>
2022-07-12 14:40:22 +0530

    initial commited

---
+++ CustomValidationSample/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomValidationSample.Models
+{
+    public class Employee
+    {
+        [Required]
+        public string MyName { get; set; }
+        [BishnuValidation(ErrorMessage ="address is required")]
+        public string Message { get; set; }
+    }
+}
